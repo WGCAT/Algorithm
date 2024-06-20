2d15187a6b7eb8fb77e08864dfeeb20b53cf0b01
WGCAT <seed9878@naver.com>
2024-06-21 02:02:07 +0900

    (feat): 막대기 추가

--- 프로그래머스/0/181847. 0 떼기/0 떼기.go
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.go
@@ -1,3 +1,5 @@
+package algorithm
+
 // 정수로 이루어진 문자열을 쪼개 슬라이스를 만들어 for문으로 왼쪽부터 0조건문 후 다시 문자열로 반환
 import (
 	"strings"
@@ -10,23 +12,23 @@ func solution(n_str string) string {
 	strSlice := strings.Split(n_str, "")
 	// 인덱스카운트 숫자 선언 및 초기화
 	index := 0
-    // for range문으로 문자열이 담긴 슬라이스를 numStr로 담아서 돌림
+	// for range문으로 문자열이 담긴 슬라이스를 numStr로 담아서 돌림
 	for _, numStr := range strSlice {
-        // numStr에 0이 속해있다면
+		// numStr에 0이 속해있다면
 		if strings.Contains(numStr, "0") {
-            // index 카운트 증가
+			// index 카운트 증가
 			index++
 
-        // numStr에 0이 속해있지 않다면    
+			// numStr에 0이 속해있지 않다면
 		} else {
-            // strSlice의 인덱스값을 index로 넣어서 index의 뒷값들을 strPut에 담기
+			// strSlice의 인덱스값을 index로 넣어서 index의 뒷값들을 strPut에 담기
 			strPut = strSlice[index:]
-            // for문 종료
-            break
+			// for문 종료
+			break
 		}
-        
+
 	}
-    // 슬라이스를 문자열로 변환
+	// 슬라이스를 문자열로 변환
 	result := strings.Join(strPut, "")
 
 	return result
