478bd08116a96e1278adad422e61df8d9afd7720
WGCAT <149450594+WGCAT@users.noreply.github.com>
2024-06-21 12:18:41 +0900

    [Silver V] Title: 막대기, Time: 4 ms, Memory: 860 KB -BaekjoonHub

--- 백준/Silver/1094. 막대기/막대기.go
+++ 백준/Silver/1094. 막대기/막대기.go
@@ -1,42 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
 func main() {
 	var X int
 	fmt.Scanln(&X)
 
-	barLen := 64
-	sumLen := barLen
-	var haveBar []int
+	// X를 2진수인 string으로 변환
+	strX := strconv.FormatInt(int64(X), 2)
 
-	for i := 0; i < 7; i++ {
-		// 막대 합계가 지민이 원하는 길이보다 크다면
-		if sumLen > X {
-			if sumLen == barLen {
-				barLen = barLen / 2
-				sumLen = barLen
-				continue
-			} else {
-				sumLen = sumLen - barLen
-			}
+	// 2진수 string을 슬라이스에 담기
+	sliceX := strings.Split(strX, "")
+	// 카운트 선언
+	count := 0
 
-			// 막대 합계가 지민이 원하는 길이보다 작다면
-		} else if sumLen < X {
-			// 슬라이스에 자른 막대 담기
-			haveBar = append(haveBar, barLen)
-
-			// 막대 합계가 지민이 원하는 길이와 같다면
-		} else if sumLen == X {
-			// 슬라이스에 자른막대 넣고 for문 빠져나오기
-			haveBar = append(haveBar, barLen)
-			break
+	// for range문으로 2진수 슬라이스를 binary에 담아 돌림
+	for _, binary := range sliceX {
+		// binary가 1이라면
+		if strings.Contains(binary, "1") {
+			// 카운트 증가
+			count++
 		}
-		barLen = barLen / 2
-		sumLen = sumLen + barLen
-
 	}
-	countBar := len(haveBar)
-	fmt.Println(countBar)
+	fmt.Println(count)
 
 }
