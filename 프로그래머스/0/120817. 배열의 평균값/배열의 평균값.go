97b1f4f13a7aa7e80de15cbda5a83728b7555496
WGCAT <seed9878@naver.com>
2024-06-22 23:51:41 +0900

    (feat): 배열의 평균값 풀이 추가

--- 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.go
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.go
@@ -1,16 +1,31 @@
+package algorithm
+
 // 정수 배열 numbers에 들어오는 값들을 더한 후 length로 나눈다
 
-func solution(numbers []int) float64 {
-    // 합계 int 변수 선언
-    var sum int
-    
-    // for range문으로 numbers의 값을 정수v에 담아 돌림
-    for _, v := range numbers {
-        sum = sum + v
-    }
-    
-    // 더해진 합계값과 numbers의 길이를 미리 float64로 형변환 시킨 후 평균값을 계산
-    average := float64(sum)/float64(len(numbers))
-    
-    return average
+func solution1(numbers []int) float64 {
+	// 합계 int 변수 선언
+	var sum int
+
+	// for range문으로 numbers의 값을 정수v에 담아 돌림
+	for _, v := range numbers {
+		sum = sum + v
+	}
+
+	// 더해진 합계값과 numbers의 길이를 미리 float64로 형변환 시킨 후 평균값을 계산
+	average := float64(sum) / float64(len(numbers))
+
+	return average
+}
+
+// numbers의 길이가 짝수이면 배열의 인덱스최소값과 인덱스최대값을 더한 값을 2로 나눈값
+// numbers의 길이가 홀수이면 배열의 중간값
+func solution2(numbers []int) float64 {
+	var result float64
+	if len(numbers)%2 == 0 {
+		result = float64((numbers[0] + numbers[len(numbers)-1])) / float64(2)
+	} else {
+		result = float64(numbers[numbers[len(numbers)-1]/2])
+	}
+	return result
+
 }
--- 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.java
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.java
@@ -1,4 +1,7 @@
-class Solution {
+// 배열의 평균값
+// numbers의 길이가 짝수이면 배열의 인덱스최소값과 인덱스최대값을 더한 값을 2로 나눈값
+// numbers의 길이가 홀수이면 배열의 중간값
+class Solution1 {
     public double solution(int[] numbers) {
         double answer = 0;
             if(numbers.length%2==0){
@@ -7,6 +10,13 @@ class Solution {
             answer = numbers[(numbers.length-1)/2];
         }
         
+        return answer;
+    }
+}
+        // 삼항연산자
+class Solution2 {
+    public double solution(int[] numbers) {
+        double answer = (numbers.length%2==0) ? (numbers[0] + numbers[numbers.length-1])/2F : numbers[(numbers.length-1)/2];
         return answer;
     }
 }
