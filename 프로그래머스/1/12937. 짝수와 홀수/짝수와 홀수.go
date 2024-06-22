64d27f9c2c2c8b58aaea1a569b440d448b2d3040
WGCAT <seed9878@naver.com>
2024-06-22 23:52:02 +0900

    (feat): 짝수와 홀수 풀이 추가

--- 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.go
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.go
@@ -1,15 +1,17 @@
+package algorithm
+
 // num이 짝수(0포함)이면 "Even", 홀수면 "Odd" 반환
 func solution(num int) string {
-    // 반환값을 담을 문자열 함수 선언
-    var response string
-    // num을 2로 나눈 나머지가 0일 경우
-    if num%2 == 0 {
-        // "Even" 반환
-        response = "Even"
-        // 나머지의 경우
-    } else {
-        // "Odd" 반환
-        response = "Odd"
-    }
-    return response
+	// 반환값을 담을 문자열 함수 선언
+	var response string
+	// num을 2로 나눈 나머지가 0일 경우
+	if num%2 == 0 {
+		// "Even" 반환
+		response = "Even"
+		// 나머지의 경우
+	} else {
+		// "Odd" 반환
+		response = "Odd"
+	}
+	return response
 }
