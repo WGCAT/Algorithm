580c536ff9c77edd2133e4fdb7400571dd3d0788
WGCAT <seed9878@naver.com>
2024-06-23 04:43:56 +0900

    (feat): 문자열 붙여서 출력하기 자바 추가

--- 프로그래머스/0/181946. 문자열 붙여서 출력하기/문자열 붙여서 출력하기.java
+++ 프로그래머스/0/181946. 문자열 붙여서 출력하기/문자열 붙여서 출력하기.java
@@ -1,11 +1,50 @@
 import java.util.Scanner;
 
-public class Solution {
+// 연산자
+public class Solution1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String a = sc.next();
         String b = sc.next();
         
         System.out.println(a+b);
+    }
+}
+// StringBuilder
+public class Solution2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+        
+        StringBuilder sb = new StringBuilder(10);
+        sb.append(a);
+        sb.append(b);
+        
+        System.out.println(sb.toString());
+    }
+}
+
+// String.concat()
+public class Solution3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+        
+        String str = a.concat(b);
+        System.out.println(str);
+    }
+}
+
+// String.format()
+public class Solution4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+        
+        String str = String.format("%s%s", a, b);
+        System.out.println(str);
     }
 }
