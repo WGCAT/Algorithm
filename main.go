33097d0f57e3ebb3b6347b68f98eaf172d6f8629
WGCAT <seed9878@naver.com>
2024-06-25 13:34:33 +0900

    (chore): 빌드 수정

--- main.go
+++ main.go
@@ -1,30 +1,44 @@
-// A ~ Z : 65 ~ 90, a ~ z : 97 ~ 122
+// 예를 들어 array가 [1, (5, 2, 6, 3), 7, 4], i = 2, j = 5, k = 3이라면
+
+// array의 2번째부터 5번째까지 자르면 [5, 2, 6, 3]입니다.
+// 1에서 나온 배열을 정렬하면 [2, 3, (5), 6]입니다.
+// 2에서 나온 배열의 3번째 숫자는 5입니다. [[2, 5, 3], [4, 4, 1], [1, 7, 3]]
 
 package main
 
 import (
 	"fmt"
-	"strings"
+	"sort"
 )
 
 func main() {
-	var s1 string
-	fmt.Scan(&s1)
+	array := []int{1, 5, 2, 6, 3, 7, 4}
+	commands := [][]int{{2, 5, 3}, {4, 4, 1}, {1, 7, 3}}
+	response := solution(array, commands)
 
-	var result string
+	fmt.Println(response)
+}
 
-	slice := strings.Split(s1, "")
+func solution(array []int, commands [][]int) []int {
+	// 결과값 담을 슬라이스 선언
+	var result []int
 
-	for _, v := range slice {
-		if v >= "A" && v <= "Z" {
-			word := string(v)
-			word = strings.ToUpper(word)
-			result = result + word
+	for _, value := range commands {
+		var sliceSort []int
+		if value[1] == len(array) {
+			for _, sliceValue := range array[value[0]-1:] {
+				sliceSort = append(sliceSort, sliceValue)
+			}
+			sort.Ints(sliceSort)
+			result = append(result, sliceSort[value[2]-1])
+
 		} else {
-			word := string(v)
-			word = strings.ToLower(word)
-			result = result + word
+			for _, sliceValue := range array[value[0]-1 : value[1]] {
+				sliceSort = append(sliceSort, sliceValue)
+			}
+			sort.Ints(sliceSort)
+			result = append(result, sliceSort[value[2]-1])
 		}
 	}
-	fmt.Println(result)
+	return result
 }
--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,32 +1,28 @@
 import java.util.*;
 
 class Solution {
-    public int[] solution(int[] array, int[][] commands) {
-        // 
-        ArrayList<Integer> answer = new ArrayList<Integer>();
+    public int[] solution(int[] array, int[][] commands) { 
+        int [] answer = new int [commands.length];
         int index = 0;
-        
+        // for each문으로 2차원배열 commands의 값들을 배열 s에 담아 돌림
         for (int[] s : commands) {
-            ArrayList<Integer> arraySort = new ArrayList<Integer>();
-            ArrayList<Integer> slice = new ArrayList<Integer>();
+            // 잘라낸 배열 array를 담기위한 배열 slice 선언
+            int [] slice = new int [s[1] - s[0] + 1];
+            // 정렬위한 배열 arraySort 선언
+            //int [] arraySort = new int [s[1] - s[0] + 1];
             
+            // for문으로 slice에 잘라낸 array 담기
             for (int j = 0; j < s[1] - s[0] + 1; j++) {
-                slice.add(j, array[s[0]-1+j]);
+                slice[j] = array[s[0]-1+j];
             }
-                
-            for (int i = 0; i<slice.size(); i++) {
-                arraySort.add(i, slice.get(i));
-            }
-                
-            Collections.sort(arraySort);
-            answer.add(index, arraySort.get(s[2]-1));
+            
+            // slice 오름차순 정렬
+            Arrays.sort(slice);
+            // answer에 slice 인덱스값 담기
+            answer[index] = slice[s[2]-1];
             index++;
         }
-        int [] result = new int [answer.size()];
-        for (int k = 0; k<answer.size(); k++) {
-            result[k] = answer.get(k);
-        }
 
-        return result;
+        return answer;
     }
 }
