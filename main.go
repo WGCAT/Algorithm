fc693e619b4ff4b15152c292bc0abb7da2554845
WGCAT <seed9878@naver.com>
2024-07-10 01:04:57 +0900

    (chore): 메인 수정

--- main.go
+++ main.go
@@ -4,79 +4,39 @@ import (
 	"fmt"
 )
 
-// [0] 0
-// [3, 4] 2
-// [1, 2, 3, 5, 6, 7, 10, 11] 5
-// [3, 5, 11, 6, 1, 5, 3, 3, 1, 41] 5
-// [1, 11, 111, 1111] 3
-
-// h번 이상 인용된 논문이 h편 이상
-// h의 최댓값이 반환
-
 func main() {
-	// lost := []int{1, 2, 3, 4, 5, 6}
-	// reserve := []int{1, 2, 3}
-	// r := solution(10, lost, reserve)
-	answers := []int{1, 2, 3, 4, 5, 6}
-	r := solution(answers)
+	a := []int{1, 2, 1, 3, 1, 4, 1, 2}
+	r := solution(a)
 	fmt.Println(r)
 
 }
 
-// {4, 6}
-// {8, 9}
-//
-//	func solution(n int, lost []int, reserve []int) int {
-//		for r := 0; r < len(reserve); r++ {
-//			if len(reserve) == 0 {
-//				break
-//			}
-//			for l := 0; l < len(lost); l++ {
-//				if len(reserve) == 0 {
-//					break
-//				} else if reserve[r] == lost[l] {
-//					lost = append(lost[:l], lost[l+1:]...)
-//					reserve = append(reserve[:r], reserve[r+1:]...)
-//					l--
-//				} else if (reserve[r]-1) == lost[l] || (reserve[r]+1) == lost[l] {
-//					lost = append(lost[:l], lost[l+1:]...)
-//					reserve = append(reserve[:r], reserve[r+1:]...)
-//					l--
-//				}
-//			}
-//		}
-//		return n - len(lost)
-//	}
-func solution(answers []int) []int {
-	var student1 = [5]int{1, 2, 3, 4, 5}
-	var student2 = [8]int{2, 1, 2, 3, 2, 4, 2, 5}
-	var student3 = [10]int{3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
-	scores := make([]int, 3)
-	var result []int
-	for i := 0; i < len(answers); i++ {
-		answer := answers[i]
-		if answer == student1[i%len(student1)] {
-			scores[0]++
+func solution(topping []int) int {
+	cs := make([]int, len(topping))
+	bro := make([]int, len(topping))
+	csCount := 0
+	broCount := 0
+	result := 0
+	for _, v := range topping {
+		if cs[v] == 0 {
+			csCount++
 		}
-		if answer == student2[i%len(student2)] {
-			scores[1]++
-		}
-		if answer == student3[i%len(student3)] {
-			scores[2]++
-		}
+		cs[v]++
 	}
-	max := 0
-	for _, score := range scores {
-		if score > max {
-			max = score
+	for _, v := range topping {
+		if bro[v] == 0 {
+			broCount++
 		}
-	}
-	for i := 0; i < len(scores); i++ {
-		if scores[i] == max {
-			result = append(result, i+1)
-		} else {
-			result = append(result, 0)
+		bro[v]++
+		cs[v]--
+		if cs[v] == 0 {
+			csCount--
 		}
+		if csCount == broCount {
+			result++
+		}
+
 	}
+
 	return result
 }
